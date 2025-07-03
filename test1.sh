7fba83413fcb36c6b56811124a4cc58430caffee
bamruthagowda <amruthagowda.b@gmail.com>
2025-07-03 17:48:39 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-row=8
-colum=8
-
-for ((i=1; i<=row; i++)); do
-for ((j=1; j<=colum; j++)); do
-sum =((i+j))
-if [[sum%2 == 0]]; then 
- echo -n " white "
-else
-echo -n " black "
-fi
-done
+for i in {2..10..2}; do
+echo "$i"
 done
 
 
