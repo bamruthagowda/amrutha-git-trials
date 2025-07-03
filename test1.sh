ca698b0e3b58bdb7d545b31138cb94bc2dcca427
bamruthagowda <amruthagowda.b@gmail.com>
2025-07-03 17:44:54 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-ls -ltr
-df -sh
-free -h 
+row=8
+colum=8
+
+for ((i=1; i<=row; i++)); do
+for ((j=1; j<=colum; j++)); do
+sum =((i+j))
+if [[sum%2 == 0]]; then 
+ echo -n " white "
+else
+echo -n " black "
+fi
+done
+done
+
+
